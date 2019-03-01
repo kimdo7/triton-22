7e32b151269c43b146fb8f25321d11488d615c6b
Kim Do <kim.h.do.seven@gmail.com>
2019-02-28 16:07:08 -0800

    add comment player.js model

---
+++ server/models/user/types/nutritionist.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var NutritionistSchema = new mongoose.Schema({
+    basicInfo: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    started_coach_year : {type: String, required: true},
+    current_team : {type: mongoose.Schema.Types.ObjectId, ref: "Team"},
+    team_coached : [{type: mongoose.Schema.Types.ObjectId, ref: "Team"}]
+}, { timestamps: true, upsert:true})
+
+mongoose.model('Nutritionist', NutritionistSchema);
--- server/models/user/types/player.js
+++ server/models/user/types/player.js
@@ -2,17 +2,36 @@
 var mongoose = require('mongoose');
 
 var PlayerSchema = new mongoose.Schema({
-    basicInfo: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    position : {type: String, required: true},
-    footing : {type: String, required: true},
-    current_team_id : {type: String, required: true},
-    started_play_year : {type: String, required: true},
-    competive_teams: [{type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
-    recreational_teams: [{type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
-    high_school: {type: mongoose.Schema.Types.ObjectId, ref: 'School'},
-    middle_school: {type: mongoose.Schema.Types.ObjectId, ref: 'School'},
-    seasons_played: [{type: mongoose.Schema.Types.ObjectId, ref: 'Season'}]
-}, { timestamps: true, upsert:true})
+    /**
+     * *BASIC INFO*
+     */
+    basicInfo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    position: { type: String, required: true },
+    footing: { type: String, required: true },
+    current_team_id: { type: String, required: true },
+    started_play_year: { type: String, required: true },
+
+    /**
+     * *TEAM*
+     */
+
+    competive_teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
+    recreational_teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
+    seasons_played: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Season' }],
+
+    /**
+     * *HIGH SCHOOL* *MIDDLE SCHOOL*
+     */
+    high_school: { type: mongoose.Schema.Types.ObjectId, ref: 'School' },
+    middle_school: { type: mongoose.Schema.Types.ObjectId, ref: 'School' },
+
+    /**
+     * *Coach* ====== *PT* ======= *Nutritionist*
+     */
+    assigned_coached: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Coach' }],
+    personal_trainer: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PersonalTrainer' }],
+    nutritionist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PersonalTrainer' }],
+}, { timestamps: true, upsert: true })
 
 mongoose.model('Player', PlayerSchema);
 
